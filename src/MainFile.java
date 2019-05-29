e562adfab28587117b575dc67488c812bbf656d1
Leoc74 <lciccarelli74@gmail.com>
2019-05-29 12:30:59 -0400

    hi

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,14 +1,33 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainFile extends Application{
     @Override
     public void start(Stage ps){
 
+        GridPane mainPane = new GridPane();
+        Pane imagePane = new Pane();
+        //Add an image of the logo
+        mainPane.add(imagePane, 0,0);
+        GridPane selectPane = new GridPane();
+        mainPane.add(selectPane, 0,1);
+        Button b1 = new Button("Test");
+        selectPane.add(b1, 0, 0);
 
 
 
+
+
+        Scene scene = new Scene(mainPane, 400, 400);
+        ps.setScene(scene);
+        ps.setTitle("Waluigi Ware");
+        ps.show();
+
+        mainPane.requestFocus();
     }
-
-
 }
