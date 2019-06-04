db546476f4e110ea27c33295246d5941c49d8e61
Leoc74 <lciccarelli74@gmail.com>
2019-06-04 14:31:58 -0400

    hi

--- src/CardClass.java
+++ src/CardClass.java
@@ -1,21 +1,41 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+
 public class CardClass {
     private Image star = new Image("star.jpg");
     private ImageView card = new ImageView(star);
     private Image front;
+    private boolean faceUp = false;
+    private boolean rectIn = false;
 
 
     public CardClass(Image front){
         this.front = front;
-        card.setOnMouseClicked(e ->card.setImage(card.getImage() == star ? front : star));
+        //card.setOnMouseClicked(e ->{
+          //  card.setImage(front);
+            //faceUp = true;
+        //});
+
 
     }
 
     public boolean sameCard(CardClass x){
         return this.front == x.front;
 
+    }
+
+    public void erase(){
+        Image white = new Image("white.jpg");
+        card.setImage(white);
+    }
+
+    public void unflip(){
+        card.setImage(star);
+    }
+
+    public boolean bothUp(CardClass c){
+        return this.isFaceUp() && c.isFaceUp();
     }
 
 
@@ -41,5 +61,17 @@ public class CardClass {
 
     public void setFront(Image front) {
         this.front = front;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    public boolean isRectIn(){
+        return rectIn;
     }
 }
--- src/Memory.java
+++ src/Memory.java
@@ -1,19 +1,22 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import java.util.concurrent.TimeUnit;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Memory extends Application {
 
     @Override
-    public void start(Stage s){
+    public void start(Stage s) {
         GridPane pane = new GridPane();
         Image wah = new Image("Waluigi.png");
         Image wah2 = new Image("Waluigi.png");
@@ -28,29 +31,47 @@ public class Memory extends Application {
         Image wario2 = new Image("wario.jpg");
 
         ArrayList<Image> imageList = new ArrayList<>();
-        imageList.add(wah);imageList.add(wah2);imageList.add(mario);imageList.add(mario2);
-        imageList.add(luigi);imageList.add(luigi2);imageList.add(wario);imageList.add(wario2);
+        imageList.add(wah);
+        imageList.add(wah2);
+        imageList.add(mario);
+        imageList.add(mario2);
+        imageList.add(luigi);
+        imageList.add(luigi2);
+        imageList.add(wario);
+        imageList.add(wario2);
+        HashMap<String, CardClass> map = new HashMap<>();
 
-        for (int i =0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
             int use = (int) (Math.random() * imageList.size());
             CardClass c1 = new CardClass(imageList.get(use));
             c1.getCard().fitWidthProperty().bind(pane.widthProperty().divide(2));
             c1.getCard().fitHeightProperty().bind(pane.heightProperty().divide(4));
             imageList.remove(use);
-            pane.add(c1.getCard(), i % 2, i/2);
+            c1.getCard().setOnMouseClicked(e -> pushImage(c1, map));
+            pane.add(c1.getCard(), i % 2, i / 2);
+
+
+            map.put(String.format("%d", i), c1);
 
         }
-
-
 
         pane.setGridLinesVisible(true);
         pane.setHgap(2);
         pane.setVgap(2);
 
+        /*
+        pane.setOnMouseClicked(e ->{
+            if (pane.getChildren().isEmpty()){
+                Text text = new Text("You Win");
+                text.xProperty().bind(pane.widthProperty().divide(2));
+                text.yProperty().bind(pane.heightProperty().divide(2));
+                text.setFill(Color.RED);
+                text.setFont(Font.font("Comic Sans", 72));
+                pane.add(text, 0, 0);
+            }
 
-
-
-
+        });
+*/
 
 
         Scene scene = new Scene(pane, 200, 400);
@@ -60,6 +81,27 @@ public class Memory extends Application {
 
         pane.requestFocus();
     }
+
+
+    public void pushImage(CardClass c1, HashMap<String, CardClass> map) {
+        c1.getCard().setImage(c1.getFront());
+        c1.setFaceUp(true);
+        for (int f = 0; f < 7; f++) {
+            for (int j = f + 1; j < 8; j++) {
+                if (map.get(String.format("%d", f)).bothUp(map.get(String.format("%d", j)))) {
+                    if (map.get(String.format("%d", f)).sameCard(map.get(String.format("%d", j)))) {
+                        map.get(String.format("%d", f)).setFaceUp(false);
+                        map.get(String.format("%d", j)).setFaceUp(false);
+                    } else {
+                        map.get(String.format("%d", f)).unflip();
+                        map.get(String.format("%d", j)).unflip();
+                    }
+                }
+            }
+        }
+
+    }
 }
+
 
 
