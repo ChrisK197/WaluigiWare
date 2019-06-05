17408bad51f37bf55d1450319f12762d10c13645
ChrisK197 <chrkal22@bergen.org>
2019-06-05 12:40:23 -0400

    Deflecter finished

--- src/MainFile.java
+++ src/MainFile.java
@@ -20,6 +20,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainFile extends Application{
+
+    protected Pane p = new Pane();
+    protected Timeline animation;
+    protected int dx = 5;
+    protected int dy = 5;
+    protected Circle shield = new Circle(150);
+    //protected Circle shield = new Circle(500, 500, 150);
+    protected Circle circle = new Circle(50);
+    protected Image loser = new Image("Waluigi5.png");
+    protected ImageView imageView2 = new ImageView(loser);
+    protected Text t = new Text("WAAAA! You lose!");
+
     @Override
     public void start(Stage ps){
 
@@ -41,6 +53,10 @@ public class MainFile extends Application{
         b2.setPrefSize(120, 10);
         selectPane.add(b2, 1, 0);
         b2.setOnMouseClicked(e -> memory(new Stage()));
+        Button b3 = new Button("Deflector");
+        b3.setPrefSize(120, 10);
+        selectPane.add(b3, 2, 0);
+        b3.setOnMouseClicked(e -> deflector(new Stage()));
 
 
 
@@ -201,6 +217,96 @@ public class MainFile extends Application{
         pane.requestFocus();
     }
 
+    public void deflector (Stage ps) {
+        shield.centerXProperty().bind(p.widthProperty().divide(2).add(10));
+        shield.centerYProperty().bind(p.heightProperty().divide(2));
+        shield.setStroke(Color.RED);
+        shield.setStrokeWidth(5);
+        shield.setFill(Color.WHITE);
+        p.getChildren().add(shield);
+
+        Image waluigi = new Image("Waluigi.png");
+        ImageView imageView = new ImageView(waluigi);
+        p.getChildren().add(imageView);
+        imageView.xProperty().bind(p.widthProperty().divide(2).subtract(waluigi.getWidth()/2));
+        imageView.yProperty().bind(p.heightProperty().divide(2).subtract(waluigi.getHeight()/2));
+
+        if ((int)(Math.random()*4) == 0) {
+            circle.setCenterX(80);
+            circle.setCenterY((int)((Math.random()*920)+80));
+        }
+        else if ((int)(Math.random()*4) == 1) {
+            circle.setCenterX(920);
+            circle.setCenterY((int)((Math.random()*920)+80));
+        }
+        else if ((int)(Math.random()*4) == 2) {
+            circle.setCenterX((int)((Math.random()*920)+80));
+            circle.setCenterY(80);
+        }
+        else {
+            circle.setCenterX((int)((Math.random()*920)+80));
+            circle.setCenterY(920);
+        }
+        circle.setFill(Color.PURPLE);
+        p.getChildren().add(circle);
+        circle.setOnMousePressed(e -> {
+            if ((int)(Math.random()*4) == 0) {
+                circle.setCenterX(80);
+                circle.setCenterY((int)((Math.random()*920)+80));
+            }
+            else if ((int)(Math.random()*4) == 1) {
+                circle.setCenterX(920);
+                circle.setCenterY((int)((Math.random()*920)+80));
+            }
+            else if ((int)(Math.random()*4) == 2) {
+                circle.setCenterX((int)((Math.random()*920)+80));
+                circle.setCenterY(80);
+            }
+            else {
+                circle.setCenterX((int)((Math.random()*920)+80));
+                circle.setCenterY(920);
+            }
+            dx *= 1.25;
+            dy *= 1.25;
+        });
+        animation = new Timeline(new KeyFrame(Duration.millis(50), e -> moveBall()));
+
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+
+        Scene scene = new Scene(p, 1000, 1000);
+        ps.setTitle("Deflector");
+        ps.setScene(scene);
+        ps.show();
+    }
+
+    protected void handleCollision() {
+        if (Math.pow(circle.getCenterX()-shield.getCenterX(), 2) + Math.pow(circle.getCenterY()-shield.getCenterY(), 2) < Math.pow(circle.getRadius()+shield.getRadius(), 2)) {
+            circle.setFill(Color.RED);
+            imageView2.setX(0);
+            imageView2.setY(0);
+            t.setFill(Color.PURPLE);
+            t.setFont(Font.font("Comic Sans", 50));
+            t.setX(300);
+            t.setY(100);
+            p.getChildren().add(imageView2);
+            p.getChildren().add(t);
+            animation.stop();
+        }
+    }
+
+    protected void moveBall() {
+        if (circle.getCenterX()<circle.getRadius() || circle.getCenterX()>p.getWidth() - circle.getRadius()) {
+            dx *= -1;
+        }
+        if (circle.getCenterY()<circle.getRadius()|| circle.getCenterY()>p.getHeight() - circle.getRadius()) {
+            dy *= -1;
+        }
+
+        handleCollision();
+        circle.setCenterX(circle.getCenterX() + dx);
+        circle.setCenterY(circle.getCenterY() + dy);
+    }
 
     public void pushImage(CardClass c1, HashMap<String, CardClass> map, GridPane pane) {
         for (int f = 0; f < 7; f++) {
