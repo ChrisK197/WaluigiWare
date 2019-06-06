9dfb80991010a1f59c4eea183de91488e2b363fa
Batman0016 <evacic22@bergen.org>
2019-06-06 12:01:37 -0400

    hi

--- src/dodgeFallingBalls2.java
+++ src/dodgeFallingBalls2.java
@@ -1,12 +1,15 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -74,7 +77,13 @@ public class dodgeFallingBalls2 extends Application {
                 if (ball.getCenterX()+30>=imageView.getX()&& ball.getCenterX()-30<=imageView.getFitWidth()+imageView.getX()){
                     if(ball.getCenterY()+30>=560 && ball.getCenterY()-30<=imageView.getY()+imageView.getFitHeight()){
                         this.stop();
-
+                        Text gameOver = new Text("Game Over");
+                        gameOver.setFill(Color.RED);
+                        gameOver.setStroke(Color.RED);
+                        pane.getChildren().add(gameOver);
+                        gameOver.setFont(Font.font("Comic Sans", 72));
+                        gameOver.setX(500);
+                        gameOver.setY(350);
                     }
                 }
                 temp = 1;
@@ -89,7 +98,13 @@ public class dodgeFallingBalls2 extends Application {
                     if (ballList.get(temp).getCenterX()+30>= imageView.getX() && ballList.get(temp).getCenterX()-30<= imageView.getFitWidth()+ imageView.getX()){
                         if(ballList.get(temp).getCenterY()+30>=560 && ballList.get(temp).getCenterY()-30<=imageView.getY()+ imageView.getFitHeight()){
                             this.stop();
-
+                            Text gameOver = new Text("Game Over");
+                            gameOver.setFill(Color.RED);
+                            gameOver.setStroke(Color.RED);
+                            pane.getChildren().add(gameOver);
+                            gameOver.setFont(Font.font("Comic Sans", 72));
+                            gameOver.setX(500);
+                            gameOver.setY(300);
                         }
                     }
                     if (ballList.get(temp).getCenterY()+30>=700){
